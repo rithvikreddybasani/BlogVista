e9a95c9ea8a05bc27420ee48e00e6bb34394f82f
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-05-09 17:22:59 +0530

    Update user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -72,9 +72,8 @@ exports.signout = async (req, res, next) => {
 };
 
 exports.getusers = async (req, res, next) => {
-  if (!req.user.isAdmin) {
-    return next(errorHandler(403, "You are not allowed to see all users"));
-  }
+  
+    
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
