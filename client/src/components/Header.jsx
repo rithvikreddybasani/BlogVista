58be2134de46a9a93e6ee127d9bdc5aa85244a5a
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-05-10 09:32:20 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -65,9 +65,9 @@ const Header = () => {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Blog
+          Inno
         </span>
-        Vista
+        Blogz
       </Link>
       <form onSubmit={handleSubmit}>
         <TextInput
